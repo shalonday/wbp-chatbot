e432b58a00d9ec1655aaa7fa89cbc20bcd73db89
shalonday <alondaysp@gmail.com>
2026-01-28 19:11:38 +0800

    fix: prevent Vite from deleting TypeScript declarations

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    emptyOutDir: false,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'WbpChatbot',
